5b5ca7833f4837064d0de946bafe22b9b363a55e
Flavio Di Natale <dinataleflavio@gmail.com>
2025-09-09 13:34:12 +0200

    Play with structs and modules
    - creation
    - struct associated methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+// Game Of Life (gol)
+mod gol {
+    #[derive(Debug)]
+    pub struct Plane {
+        width: i32,
+        length: i32,
+    }
+
+    impl Plane {
+        pub fn new(width: i32, length: i32) -> Self {
+            Self { width, length }
+        }
+
+        pub fn area(&self) -> i32 {
+            self.width * self.length
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let width = 10;
+    let length = 10;
+
+    let current_plane = gol::Plane::new(width, length);
+    let next_plane = gol::Plane::new(width, length);
+
+    dbg!(&current_plane);
+    dbg!(&next_plane);
+
+    println!("Current plane area is {}.", current_plane.area())
 }
