f2851a5358308f9810fc77db38d774d55bf64c4d
Flavio Di Natale <dinataleflavio@gmail.com>
2025-09-09 14:41:30 +0200

    draft general structure of the program

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,59 @@
 // Game Of Life (gol)
 mod gol {
-    #[derive(Debug)]
+    // Struct representing a Plane
     pub struct Plane {
         width: i32,
         length: i32,
+        current: Vec<Vec<CellState>>,
+        next: Vec<Vec<CellState>>,
+    }
+
+    pub enum CellState {
+        Alive,
+        Dead,
+    }
+
+    fn new_matrix(width: i32, length: i32) -> Vec<Vec<CellState>> {
+        let mut matrix = Vec::new();
+
+        for _ in 0..length {
+            let mut row = Vec::new();
+
+            for _ in 0..width {
+                row.push(CellState::Dead);
+            }
+
+            matrix.push(row);
+        }
+        return matrix;
     }
 
     impl Plane {
         pub fn new(width: i32, length: i32) -> Self {
-            Self { width, length }
+            Self {
+                width,
+                length,
+                current: new_matrix(width, length),
+                next: new_matrix(width, length),
+            }
         }
 
-        pub fn area(&self) -> i32 {
-            self.width * self.length
+        // fn trim_coords(x, y) -> (i32, i32)
+
+        // fn count_alive_neighbors(x, y) -> u8
+
+        // fn evolve(&self)
+
+        pub fn print(&self) {
+            for row in &self.current {
+                for item in row {
+                    match item {
+                        CellState::Alive => print!("X"),
+                        CellState::Dead => print!(" "),
+                    }
+                }
+                println!();
+            }
         }
     }
 }
@@ -21,11 +62,20 @@ fn main() {
     let width = 10;
     let length = 10;
 
-    let current_plane = gol::Plane::new(width, length);
-    let next_plane = gol::Plane::new(width, length);
+    let plane = gol::Plane::new(width, length);
 
-    dbg!(&current_plane);
-    dbg!(&next_plane);
+    // plane.set_cell(x, y, alive or dead);
 
-    println!("Current plane area is {}.", current_plane.area())
+    let mut count = 0;
+    loop {
+        // clear screen
+        plane.print();
+
+        // plane.evolve()
+        count += 1;
+        if count == 10 {
+            break;
+        }
+        // wait
+    }
 }
