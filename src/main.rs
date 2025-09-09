07a2e6b2566f59e618fd778d77b3c83931815ba7
Flavio Di Natale <dinataleflavio@gmail.com>
2025-09-09 15:13:10 +0200

    implement wrapping plane

--- src/main.rs
+++ src/main.rs
@@ -38,11 +38,31 @@ mod gol {
             }
         }
 
-        pub fn set_cell(&mut self, x: usize, y: usize, state: CellState) {
+        pub fn set_cell(&mut self, x: i32, y: i32, state: CellState) {
+            let (x, y) = self.trim_coords(x, y);
+
             self.current[y][x] = state;
         }
 
-        // fn trim_coords(x, y) -> (i32, i32)
+        fn trim_coords(&self, x: i32, y: i32) -> (usize, usize) {
+            let x = if x >= self.width {
+                x % self.width
+            } else if x < 0 {
+                self.width - (-x % self.width)
+            } else {
+                x
+            };
+
+            let y = if y >= self.length {
+                y % self.length
+            } else if y < 0 {
+                self.length - (-y % self.length)
+            } else {
+                y
+            };
+
+            (x as usize, y as usize)
+        }
 
         // fn count_alive_neighbors(x, y) -> u8
 
@@ -68,12 +88,15 @@ fn main() {
 
     let mut plane = gol::Plane::new(width, length);
 
+    // try to set cell out of index
+    plane.set_cell(-1, 12, gol::CellState::Alive);
+
     // draw a glider
-    plane.set_cell(4, 0, gol::CellState::Alive);
-    plane.set_cell(5, 0, gol::CellState::Alive);
-    plane.set_cell(6, 0, gol::CellState::Alive);
-    plane.set_cell(6, 1, gol::CellState::Alive);
-    plane.set_cell(5, 2, gol::CellState::Alive);
+    // plane.set_cell(4, 0, gol::CellState::Alive);
+    // plane.set_cell(5, 0, gol::CellState::Alive);
+    // plane.set_cell(6, 0, gol::CellState::Alive);
+    // plane.set_cell(6, 1, gol::CellState::Alive);
+    // plane.set_cell(5, 2, gol::CellState::Alive);
 
     let mut count = 0;
     loop {
