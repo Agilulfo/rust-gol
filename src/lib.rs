55f92671a62a2057f954d90f51511fc7d886ed25
Flavio Di Natale <dinataleflavio@gmail.com>
2025-09-11 21:17:19 +0200

    Add potential TODOs

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub mod utils;
 pub struct Plane {
     width: i32,
     length: i32,
+    // TODO: is there a way to define this as a type and reuse it where needed?
     current: Vec<Vec<CellState>>,
     next: Vec<Vec<CellState>>,
 }
@@ -56,6 +57,8 @@ impl Plane {
         let mut rng = rand::rng();
         for x in 0..self.width {
             for y in 0..self.length {
+                // TODO: how can I avoid having to cast to usize is it possible?
+                // TODO: would be nice to generate directly one of the options, is this even possible?
                 self.current[y as usize][x as usize] = if rng.random() {
                     CellState::Alive
                 } else {
@@ -71,6 +74,7 @@ impl Plane {
         self.current[y][x] = state;
     }
 
+    // TODO: can this be tested and remain private?
     fn trim_coords(&self, x: i32, y: i32) -> (usize, usize) {
         let x = if x >= self.width {
             x % self.width
@@ -114,11 +118,19 @@ impl Plane {
         return count;
     }
 
+    // TODO: would be nice to implement a detection of a stall
+    // we could keep a track of the recent n states (via hash)
+    // and if an hash repeats we could somehow terminate the execution
     pub fn evolve(&mut self) {
         for x in 0..self.width {
             for y in 0..self.length {
                 let alive_neighbors = self.count_alive_neighbors(x, y);
-
+                // TODO: maybe there is a way to write this that is easier to read
+                // rules from wikipedia:
+                //  - Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+                //  - Any live cell with two or three live neighbours lives on to the next generation.
+                //  - Any live cell with more than three live neighbours dies, as if by overpopulation.
+                //  - Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
                 let next_state = match self.current[y as usize][x as usize] {
                     CellState::Alive => {
                         if alive_neighbors == 2 || alive_neighbors == 3 {
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,7 @@
 use std::thread;
 use std::time::Duration;
 
+// TODO: is it possible to use an escape sequence that works also on emacs window?
 pub fn clear_terminal() {
     print!("\x1B[2J\x1B[1;1H");
 }
